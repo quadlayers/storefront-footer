f86905cf9ab06548b4915e986665b4326c92582f
Francisco <francisco.mastromarino@urbiport.com>
2025-12-03 09:30:08 -0300

    fix: wordpress compatibility

--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Donate link: https://quadlayers.com/
 Tags: storefront footer, storefront credit, storefront footer text, storefront footer copyright, storefront
 Requires at least: 4.7
 Requires PHP: 5.6
-Tested up to: 6.8
-Stable tag: 2.3.1
+Tested up to: 6.9
+Stable tag: 2.3.2
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 4.0
@@ -28,6 +28,9 @@ Works only with [Storefront Theme](https://wordpress.org/themes/storefront).
 1. Click "Install"
 
 == Changelog ==
+
+= 2.3.2 =
+* WordPress compatibility
 
 = 2.3.1 =
 * WooCommerce compatibility
--- storefront-footer.php
+++ storefront-footer.php
@@ -5,14 +5,14 @@
  * Plugin Name:             Change Storefront Footer Copyright Text
  * Plugin URI:              https://quadlayers.com
  * Description:             Allows you to change the official Woocommerce Storefront theme footer copyright credit text.
- * Version:                 2.3.1
+ * Version:                 2.3.2
  * Text Domain:             storefront-footer
  * Author:                  QuadLayers
  * Author URI:              https://quadlayers.com
  * License:                 GPLv3
  * Domain Path:             /languages
  * Request at least:        4.7
- * Tested up to:            6.8
+ * Tested up to:            6.9
  * Requires PHP:            5.6
  * WC requires at least:    4.0
  * WC tested up to:         10.3
